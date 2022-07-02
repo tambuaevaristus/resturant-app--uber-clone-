db6a20e26f288ef90aacf721a8732d07b9370636
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-02 13:43:48 +0100

    yelp api faild

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
@@ -6,9 +6,41 @@ import RestaurantItems, {
   localRestaurants,
 } from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
+const YELP_API_KEY="bdRJutLhFAQJ36t7b89CWjHFBU4OKzjt9wvZzcY-nkgmvTqlNMjZWV1eG7iBQ9R74SyfxRg9LWnBAkZY06BtAZAe4d2dfX-2vuX8a1l5V7foctHfX9UKEyoM5ts3YXYx"
+
 
 export default function Home() {
     const [ restaurantData, setRestaurantData ] = useState(localRestaurants);
+    const [city, setCity] = useState("San Francisco");
+    const [activeTab, setActiveTab] = useState("Delivery");
+
+    const getRestaurantsFromYelp = () => {
+
+        
+        // This is not your API key boy
+        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+    
+        const apiOptions = {
+          headers: {
+            Authorization: `Bearer ${YELP_API_KEY}`,
+          },
+        };
+    
+        return fetch(yelpUrl, apiOptions)
+          .then((res) => res.json())
+          .then((json) =>
+            setRestaurantData(
+              json.businesses.filter((business) =>
+                business.transactions.includes(activeTab.toLowerCase())
+              )
+            )
+          );
+      };
+
+      useEffect(() => {
+            getRestaurantsFromYelp();
+      }, []);
+    
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
