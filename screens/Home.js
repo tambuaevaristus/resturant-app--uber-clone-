7a4ff9c0b444e1fe3eb7d599f9658634096031ed
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-02 13:17:30 +0100

    local data fine

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
-import RestaurantItems from "../components/RestaurantItems";
+import RestaurantItems, {
+  localRestaurants,
+} from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
+    const [ restaurantData, setRestaurantData ] = useState(localRestaurants);
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
@@ -14,10 +17,7 @@ export default function Home() {
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
-        <RestaurantItems />
-       
-
-
+        <RestaurantItems restaurantData={restaurantData} />
       </ScrollView>
     </SafeAreaView>
   );
