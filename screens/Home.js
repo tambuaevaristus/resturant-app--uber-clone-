9c1d452f4172d8a4be3eace7b774cdb6c81a5c6d
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-04 17:00:35 +0100

    wahala de

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
@@ -6,48 +6,47 @@ import RestaurantItems, {
   localRestaurants,
 } from "../components/RestaurantItems";
 import SearchBar from "../components/SearchBar";
-const YELP_API_KEY="yfa_z3k_0tioMuD2zKPH0RhtYLn8CnLhg8uxtbBimeGWHK04aubE2wKJtdeMn1LK4wGGCR1ILPve_2v-uZapw8CXfqUkXW30sIKLoirygKFekefO6KoILiV766PAYnYx"
+const YELP_API_KEY =
+  "yfa_z3k_0tioMuD2zKPH0RhtYLn8CnLhg8uxtbBimeGWHK04aubE2wKJtdeMn1LK4wGGCR1ILPve_2v-uZapw8CXfqUkXW30sIKLoirygKFekefO6KoILiV766PAYnYx";
 
 export default function Home() {
-    const [ restaurantData, setRestaurantData ] = useState(localRestaurants);
-    const [city, setCity] = useState("");
-    const [activeTab, setActiveTab] = useState("Delivery");
+  const [restaurantData, setRestaurantData] = useState(localRestaurants);
+  const [city, setCity] = useState("");
+  const [activeTab, setActiveTab] = useState("Delivery");
 
-    console.log("home==> "+ city);
+  console.log("home==> " + city);
 
-    const getRestaurantsFromYelp = () => {
+  const getRestaurantsFromYelp = () => {
+    // This is not your API key boy
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
 
+    console.log(yelpUrl);
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
 
-        // This is not your API key boy
-        const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
-    
-        console.log(yelpUrl)
-        const apiOptions = {
-          headers: {
-            Authorization: `Bearer ${YELP_API_KEY}`,
-          },
-        };
-    
-        return fetch(yelpUrl, apiOptions)
-          .then((res) => res.json())
-          .then((json) =>
-            setRestaurantData(
-              json.businesses.filter((business) =>
-                business.transactions.includes(activeTab.toLowerCase())
-              )
-            )
-          );
-      };
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) =>
+        setRestaurantData(
+          json.businesses.filter((business) =>
+            business.transactions.includes(activeTab.toLowerCase())
+          )
+        )
+      );
+  };
 
-      useEffect(() => {
-            getRestaurantsFromYelp();
-      }, []);
-    
+  useEffect(() => {
+    getRestaurantsFromYelp();
+  }, [city]);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
-        <SearchBar cityHandler={setCity} /> 
+        <SearchBar cityHandler={setCity} />
       </View>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
