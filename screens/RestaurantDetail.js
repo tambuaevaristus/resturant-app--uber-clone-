a8ecaa3d37e1e027c9fce64846a07deb5d71d10f
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-09 12:42:34 +0100

    cool update

--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -1,20 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
 import About from "../components/restaurantDetails/About";
-import { Divider } from 'react-native-elements'
+import { Divider } from "react-native-elements";
 import MenuItems from "../components/restaurantDetails/MenuItems";
+import ViewCart from "../components/restaurantDetails/ViewCart";
 // import { Divider } from "@rneui/themed";
 
-export default function RestaurantDetail({route}) {
+export default function RestaurantDetail({ route, navigation }) {
   const routeParams = route.params;
 
-  console.log("routeparams :  >"+ routeParams.name);
   return (
     <View>
-      <About route={routeParams? routeParams : { }}/>
-       <Divider width={1.8} style={{marginVertical:20}} />
+      <About style={{}} route={routeParams ? routeParams : {}} />
+      <Divider width={1.8} style={{ marginVertical: 20 }} />
 
-       <MenuItems />
+      <MenuItems />
+      <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   );
 }
