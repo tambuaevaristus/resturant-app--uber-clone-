35fbbe0517cfb32c3dfe269b7e4235bbc41778a1
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-17 23:53:00 +0100

    sunday work

--- components/restaurantDetails/MenuItems.js
+++ components/restaurantDetails/MenuItems.js
@@ -114,7 +114,7 @@ const FoodInfo = (props) => (
   <View style={{ width: 240, justifyContent: "space-evenly" }}>
     <Text style={styles.titleStyle}>{props.food.title}</Text>
     <Text>{props.food.description}</Text>
-    <Text>{props.food.price}</Text>
+    {/* <Text>{props.food.price}</Text> */}
   </View>
 );
 
